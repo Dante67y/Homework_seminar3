fe2f4938176c7deb95e7f1c99c8627750a172e11
Dante67yt <dante67yt@gmail.com>
2022-07-05 22:12:35 +0300

    add task 3

---
+++ task_3/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на вход число N и выдаёт таблицу кубов чисел от 1 до N
+
+// int n;
+// n = Convert.ToInt32(Console.ReadLine());
+// for (int i = 1; i <= n; i++)
+// {
+//     Console.WriteLine(i * i * i);
+// }
+
+int n, i = 1;
+n = Convert.ToInt32(Console.ReadLine());
+while (i <= n)
+{
+    Console.WriteLine(i * i * i);
+    i++;
+}

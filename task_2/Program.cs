8f474045a64f21499fe43e2febb34f996328a5f2
Dante67yt <dante67yt@gmail.com>
2022-07-09 00:45:54 +0300

    change task 2

--- task_2/Program.cs
+++ task_2/Program.cs
@@ -7,5 +7,6 @@ z = Convert.ToDouble(Console.ReadLine());
 x1 = Convert.ToDouble(Console.ReadLine());
 y1 = Convert.ToDouble(Console.ReadLine());
 z1 = Convert.ToDouble(Console.ReadLine());
-len = Math.Cbrt((x - x1) * (x - x1) * (x - x1) + (y - y1) * (y - y1) * (y - y1) + (z - z1) * (z - z1) * (z - z1));
+// len = Math.Cbrt((x - x1) * (x - x1) * (x - x1) + (y - y1) * (y - y1) * (y - y1) + (z - z1) * (z - z1) * (z - z1));
+len = Math.Sqrt((x - x1) * (x - x1) + (y - y1) * (y - y1) + (z - z1) * (z - z1));
 Console.WriteLine(len);
